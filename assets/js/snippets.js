5105d5b57e7963c84bc509acb6c2318d420830ae
Agung Riyanto <4gungriyant0@gmail.com>
2023-06-29 19:01:26 +0700

    Add files via upload

---
+++ assets/js/snippets.js
@@ -0,0 +1,12 @@
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show();
+  });
+}
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
